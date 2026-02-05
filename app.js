198a535173bbe44e9825f865588663d1a132ab98
Riaan Harwood <riaan.harwood@fisherpaykel.com>
2026-02-05 15:12:18 +1300

    Testing debugging methods in app.js
    Testing various debugging methods in app.js. Methods include: chalk, debug and morgan

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express'); 
+const chalk = require('chalk'); 
+const debug = require('debug')('app'); 
 const app = express(); 
+
 
 app.get('/', (req, res) =>{
     res.send('Hello from my app'); 
 }); 
 
 app.listen(3000, ()=>{
-    console.log('listening on port 3000'); 
+    debug(`listening on port ${chalk.green('3000')}`); 
 }); 
 
 
