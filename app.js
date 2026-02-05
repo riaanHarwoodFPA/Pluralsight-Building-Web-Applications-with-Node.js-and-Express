721c623fbe5532f8edfc043d44427eb1a9539f64
Riaan Harwood <riaan.harwood@fisherpaykel.com>
2026-02-05 15:43:05 +1300

    Index testing
    Index testing

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const express = require('express');
 const chalk = require('chalk'); 
 const debug = require('debug')('app'); 
 const app = express(); 
+const morgan = require('morgan'); 
+const path = require('path'); 
 
 
-app.get('/', (req, res) =>{
-    res.send('Hello from my app'); 
-}); 
+app.use(morgan('tiny')); 
+app.use(express.static(path.join(__dirname, '/public/'))); 
+
+// When testing indexing 
+
+// app.get('/', (req, res) =>{
+//     res.send('Hello from my app'); 
+//});
 
 app.listen(3000, ()=>{
     debug(`listening on port ${chalk.green('3000')}`); 
 }); 
+
+
 
 
