d18015a22fd02ab9b12f471dbbcfc957f12aa501
Riaan Harwood <104803762+riaanHarwood@users.noreply.github.com>
2026-02-18 17:39:01 +1300

    updates sessionsRouter and sessions.ejs
    updates sessionsRouter and sessions.ejs

--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -1,6 +1,6 @@
 const express = require('express'); 
 const debug = require('debug')('app:sessionRouter'); 
-const { MongoClient } = require('mongodb'); 
+const { MongoClient, ObjectId } = require('mongodb');
 const sessions = require('../data/sessions.json');
 
 const sessionsRouter = express.Router(); 
@@ -33,9 +33,31 @@ sessionsRouter.route('/').get((req, res) => {
 
 sessionsRouter.route('/:id').get((req, res) => {
     const id = req.params.id;
-    res.render('session', { 
-        session: sessions[id], 
-    }); 
+    const url = process.env.MONGODB_URI;
+    const dbName = process.env.MONGODB_DBNAME;
+
+    (async function mongo(){
+        let client; 
+        try{
+            client = new MongoClient(url);
+            await client.connect(); // important
+            debug('Connected to the mongo DB'); 
+
+            const db = client.db(dbName); 
+            const session = await db.collection('sessions').findOne({ _id: new ObjectId(id) });
+            res.render('session', { 
+                session,
+            });
+        } catch (error){
+            debug(error.stack);
+        }
+        finally {
+            if (client) {
+                await client.close();
+            }
+        }
+    })();
+
 });
 
 module.exports = sessionsRouter; 
