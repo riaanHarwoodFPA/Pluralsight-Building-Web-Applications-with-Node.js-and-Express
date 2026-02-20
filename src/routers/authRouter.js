73aca21ecb755300ed22b22ecdee9a31d69fa165
Riaan Harwood <104803762+riaanHarwood@users.noreply.github.com>
2026-02-20 17:22:54 +1300

    added authetication and connected user registration to mongodb
    added authetication and connected user registration to mongodb

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -6,16 +6,43 @@ const { MongoClient, ObjectId } = require('mongodb');
 const authRouter = express.Router();
 
 authRouter.route('/signUp').post((req, res) => {
-    //TODO create user
-    req.login(req.body, () =>{
-        res.redirect('/auth/profile'); 
-    });
+
+    const { username, password } = req.body;
+    const url = process.env.MONGODB_URI;
+    const dbName = process.env.MONGODB_DBNAME;
+
+    (async function addUser(){
+        let client; 
+        try{
+            client = await MongoClient.connect(url);
+            const db = client.db(dbName); 
+
+            const user = { username, password };  
+            
+            const results = await db.collection('users').insertOne(user); 
+            debug(results); 
+
+            req.login(user, () => {
+                res.redirect('/auth/profile'); 
+            });
+
+        } catch (error){
+            debug(error);
+            res.status(500).send("Error creating user");
+        } finally {
+            if (client) {
+                await client.close();
+            }
+        }
+    })();  
 });
+
 
 authRouter.route('/profile').get((req, res) => {
     res.json(req.user);
 }); 
 
 module.exports = authRouter; 
+
 
 
